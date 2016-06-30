118d2dc15a8277e465b9c994de6e6d860775de02
Thomas-Franklin <t.franklin@kainos.com>
2016-06-30 13:30:24 +0100

    Commiting my changes to my branch

--- FourLoopMVC/src/mvc_course/controllers/WorldController.java
+++ FourLoopMVC/src/mvc_course/controllers/WorldController.java
@@ -6,37 +6,49 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mvc_data.IEmployeeMapper;
 import mvc_data.IWorldMapper;
 
 @Controller
 public class WorldController {
 	// testing git integration
 	@Autowired
-	public IWorldMapper worldMapper;
+	public IEmployeeMapper empMapper;
+
 	
-	@RequestMapping("/continentList.mvc")
-	public String continents(Model m) {
-		m.addAttribute("continents", worldMapper.getContinents());
-		return "continents";
+	String name = "Thomas";
+	
+	@RequestMapping("/insertEmployee.mvc")
+	public String employee(Model m) {
+		
+		empMapper.insertEmployees("Paul");
+		
+		return "employee";
 	}
-	
-	
-	@RequestMapping("/{cont}/regions.mvc")
-	public String regions(Model m, @PathVariable("cont") String continent) {
-		
-		m.addAttribute("regions", worldMapper.getRegionsForContinent(continent));
-		m.addAttribute("continent", continent);
-		
-		return "regions";
-	}
-	
-	@RequestMapping("/{region}/countries.mvc")
-	public String countries(Model m, @PathVariable String region) {
-		
-		m.addAttribute("countries", worldMapper.getCountriesForRegion(region));
-		m.addAttribute("region", region);
-		
-		return "countries";
-	}
+//	
+//	@RequestMapping("/continentList.mvc")
+//	public String continents(Model m) {
+//		m.addAttribute("continents", worldMapper.getContinents());
+//		return "continents";
+//	}
+//	
+//	
+//	@RequestMapping("/{cont}/regions.mvc")
+//	public String regions(Model m, @PathVariable("cont") String continent) {
+//		
+//		m.addAttribute("regions", worldMapper.getRegionsForContinent(continent));
+//		m.addAttribute("continent", continent);
+//		
+//		return "regions";
+//	}
+//	
+//	@RequestMapping("/{region}/countries.mvc")
+//	public String countries(Model m, @PathVariable String region) {
+//		
+//		m.addAttribute("countries", worldMapper.getCountriesForRegion(region));
+//		m.addAttribute("region", region);
+//		
+//		return "countries";
+//	}
 	
 }
