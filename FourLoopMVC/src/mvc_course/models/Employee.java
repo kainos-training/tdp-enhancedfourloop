e85be7016ef32bf5ca23072decd612bd5333df56
Thomas-Franklin <t.franklin@kainos.com>
2016-06-30 17:06:44 +0100

    t

---
+++ FourLoopMVC/src/mvc_course/models/Employee.java
@@ -0,0 +1,35 @@
+package mvc_course.models;
+
+
+// pojo for employee
+public class Employee {
+
+	private String fname, lname, jobRole;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getJobRole() {
+		return jobRole;
+	}
+
+	public void setJobRole(String jobRole) {
+		this.jobRole = jobRole;
+	}
+	
+	
+	
+}
